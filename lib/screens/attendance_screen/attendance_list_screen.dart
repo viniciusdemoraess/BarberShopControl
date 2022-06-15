ad151060e572f2b84b8b6c07cec858b73b580f44
viniciusdemoraess <demoraesvini@outlook.com>
2022-06-14 22:38:00 -0400

    fix: Finish correction list reserves

--- lib/screens/attendance_screen/attendance_list_screen.dart
+++ lib/screens/attendance_screen/attendance_list_screen.dart
@@ -4,6 +4,7 @@ import 'package:barber_shop_control/services/attendance_service.dart';
 import 'package:barber_shop_control/services/func_service.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class AttendanceListScreen extends StatefulWidget {
   final UserLocal? userLocal;
@@ -20,67 +21,86 @@ class _AttendanceListScreenState extends State<AttendanceListScreen> {
 
     String nameFun;
 
-    return StreamBuilder<QuerySnapshot>(
-        stream: attedanceService.getAttendanceList(),
-        builder: (BuildContext ctx, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          } else if (snapshot.hasData) {
-            List<DocumentSnapshot> docSnap = snapshot.data!.docs;
-            return Padding(
-              padding: const EdgeInsets.all(5.0),
-              child: ListView.separated(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                itemBuilder: (ctx, index) {
-                  FuncService funcService = FuncService();
-                  funcService
-                      .getById(docSnap[index].get('refFunc'))
-                      .then((value) {
-                    setState(() {
-                      nameFun = value;
+    return Card(
+      color: const Color.fromARGB(255, 202, 235, 248),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      child: StreamBuilder<QuerySnapshot>(
+          stream: attedanceService.getAttendanceList(),
+          builder: (BuildContext ctx, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              List<DocumentSnapshot> docSnap = snapshot.data!.docs;
+              return Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: ListView.separated(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  itemBuilder: (ctx, index) {
+                    FuncService funcService = FuncService();
+                    funcService
+                        .getById(docSnap[index].get('refFunc'))
+                        .then((value) {
+                      setState(() {
+                        nameFun = value;
+                      });
                     });
-                  });
-              
-                  return Card(
-                    color: const Color.fromARGB(255, 202, 235, 248),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          child: FutureBuilder(
-                              future: funcService
-                                  .getById(docSnap[index].get('refFunc')),
-                              builder: (BuildContext context,
-                                  AsyncSnapshot<String> text) {
-                                return text.hasData
-                                    ? Text(text.data!)
-                                    : Container();
-                              }),
+                    DateTime _selectedDate = (docSnap[index].get('date')).toDate();
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                        color: Color.fromARGB(255, 190, 201, 209),
+                        child: Padding(
+                          padding: const EdgeInsets.all(4.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly ,
+                            children: [
+                              Expanded(
+                                child: FutureBuilder(
+                                    future: funcService
+                                        .getById(docSnap[index].get('refFunc')),
+                                    builder: (BuildContext context,
+                                        AsyncSnapshot<String> text) {
+                                      return text.hasData
+                                          ? Text(text.data!)
+                                          : Container();
+                                    }),
+                              ),
+                              // Text(nameFun),
+                              Expanded(
+                                  child: Text(docSnap[index].get('type').toString())),
+                              Expanded(
+                                  child: Text(DateFormat('dd-MM-yy').format(_selectedDate)),),
+                              Expanded(
+                                  child: IconButton(
+                                      icon: const Icon(
+                                        Icons.delete,
+                                        color: Colors.red,
+                                      ),
+                                      onPressed: () {
+                                        attedanceService
+                                            .deleteAttedance(docSnap[index].id);
+                                      })),
+                            ],
+                          ),
                         ),
-                        // Text(nameFun),
-                        Expanded(
-                            child: Text(docSnap[index].get('type').toString())),
-                        Expanded(
-                            child: Text(docSnap[index].get('date').toString())),
-                      ],
-                    ),
-                  );
-                },
-                separatorBuilder: (ctx, index) {
-                  return const SizedBox(
-                    height: 5,
-                  );
-                },
-                itemCount: docSnap.length,
-              ),
-            );
-          } else {
-            return const Center(child: Text('Dados Indisponíveis!'));
-          }
-        });
+                      ),
+                    );
+                  },
+                  separatorBuilder: (ctx, index) {
+                    return const SizedBox(
+                      height: 5,
+                    );
+                  },
+                  itemCount: docSnap.length,
+                ),
+              );
+            } else {
+              return const Center(child: Text('Dados Indisponíveis!'));
+            }
+          }),
+    );
   }
 }
--- lib/screens/home_screen/home_reserve.dart
+++ lib/screens/home_screen/home_reserve.dart
@@ -23,7 +23,7 @@ class _HomeReserveScreenState extends State<HomeReserveScreen> {
           children: [
             const SizedBox(height: 15),
             const Text(
-              'Minha Agenda',
+              'Meus Horários',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
@@ -56,31 +56,60 @@ class _HomeReserveScreenState extends State<HomeReserveScreen> {
                                 borderRadius: const BorderRadius.only(
                                   topLeft: Radius.circular(5.0),
                                   topRight: Radius.circular(5.0),
-                                  bottomLeft: Radius.circular(5.0),
-                                  bottomRight: Radius.circular(5.0),
+                                  // bottomLeft: Radius.circular(5.0),
+                                  // bottomRight: Radius.circular(5.0),
                                 ),
                               ),
                               child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
                                 children: [
-                                  Expanded(
-                                    child: OutlinedButton(
-                                        style: OutlinedButton.styleFrom(
-                                          primary: Colors.blue,
-                                          backgroundColor:
-                                              const Color(0xFFEFF7F6),
-                                        ),
-                                        onPressed: () {
-                                          Navigator.of(context).push(
-                                              MaterialPageRoute(
-                                                  builder: (context) =>
-                                                      AttendanceAddScreen(
-                                                          userLocal: userService
-                                                              .userLocal)));
-                                        },
-                                        child: const Text("Solicitar Reserva")),
-                                  ),
+                                  OutlinedButton(
+                                      style: OutlinedButton.styleFrom(
+                                        primary: Colors.blue,
+                                        backgroundColor:
+                                            const Color(0xFFEFF7F6),
+                                      ),
+                                      onPressed: () {
+                                        Navigator.of(context).push(
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    AttendanceAddScreen(
+                                                        userLocal: userService
+                                                            .userLocal)));
+                                      },
+                                      child: const Text("Solicitar Serviço")),
                                   const SizedBox(width: 5),
                                 ],
+                              ),
+                            ),
+                            Text('${userService.userLocal?.email}'),
+                            // Text('IsAdmin ? ${userService.userLocal?.admin}'),
+                            Padding(
+                              padding: const EdgeInsets.all(
+                                  7.0),
+                              child: Container(
+                                decoration: const BoxDecoration(
+                                  color: Colors.blueGrey,
+                                  borderRadius: BorderRadius.only(
+                                    topRight: Radius.circular(4),
+                                    topLeft: Radius.circular(4),
+                                    bottomLeft:Radius.circular(4) ,
+                                    bottomRight: Radius.circular(4),
+                                  ),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.only(left: 8.0),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: const [
+                                      Expanded(child: Text('Funcionary', style: TextStyle(color: Colors.white),)),
+                                      Expanded(child: Text('Serviço', style: TextStyle(color: Colors.white),)),
+                                      Expanded(child: Text('Data', style: TextStyle(color: Colors.white),)),
+                                      Expanded(child: Text('Delete', style: TextStyle(color: Colors.white),)),
+                                    ],
+                                  ),
+                                ),
                               ),
                             ),
                             Expanded(
